7f22152b253ecf96e2b23f62aa8dd29249b488c9
EdwinJ-19 <edwinjames2001@gmail.com>
2024-03-03 13:28:46 +0530

    index_completed

--- zoomproject/zoomapp/urls.py
+++ zoomproject/zoomapp/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('cart/',views.cart,name='cart'),
+    path('login/',views.login,name='login'),
+    path('register/',views.register,name='register'),
 ]
--- zoomproject/zoomapp/views.py
+++ zoomproject/zoomapp/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'index.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
